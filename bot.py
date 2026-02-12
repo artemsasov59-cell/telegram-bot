cfc87e893bb4ef40218baaf4cbe395ad86adce3c
artemsasov59-cell <artemsasov59@gmail.com>
2026-02-12 02:53:20 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,22 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters, CommandHandler
 
-TOKEN = os.environ.get("8282371677:AAEfj9kJVYYm0AkV-x3UZ2WYoqLor-ccaEo")
+TOKEN = os.getenv("8282371677:AAEfj9kJVYYm0AkV-x3UZ2WYoqLor-ccaEo")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Бот работает ✅")
 
 async def auto_reply(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.message:
-        await update.message.reply_text(
-            "Привет! 👋\n"
-            "Я сейчас не онлайн, отвечу позже 😊\n\n"
-            "📌 Прайс: https://t.me/AnonimHAB/23"
-        )
+    await update.message.reply_text(
+        "Привет! 👋\n\n"
+        "Я сейчас не в сети, отвечу позже 😊\n\n"
+        "📌 Прайс: https://t.me/AnonimHAB/23"
+    )
 
 app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, auto_reply))
+
 app.run_polling()
